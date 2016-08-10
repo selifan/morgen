9e25d20148b9662f9b9e5a3a558e4e7da7a8adb3
Alexander Selifonov <as-works@narod.ru>
2016-08-10 17:15:36 +0300

    demo files

--- demo/morgen-demo.php
+++ demo/morgen-demo.php
@@ -1,1 +1,19 @@
 <?php
+/**
+* @name morgen-demo.php
+* Demonstrating functionality of morgen.php
+* @author Alexander Selifonov
+*/
+
+
+include_once('../src/morgen.php');
+
+$generator = new \Morgen\IconGenerator();
+$generator->setSvgConvertor('D:/app/inkscape/inkscape.exe -z {from} -e {to}');
+
+$options = array(
+  'project' => 'project-greatApp.xml',
+  'forced'  => true
+);
+
+$generator->createIconsFromImages($options);
